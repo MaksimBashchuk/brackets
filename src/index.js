88a7d0b538eef62ce1a11cd765113ee2cfec930e
Maksim Bashchuk <MaksimBashchuk@gmail.com>
2020-10-06 17:54:32 +0300

    init: start breckets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+module.exports = 
+function check(str, bracketsConfig) {
+  let bracketsStr = [];
+  bracketsConfig.forEach(i => bracketsStr.push(i.join('')));
+
+  bracketsStr.forEach(i => {
+    while (str.includes(i)) {
+      str = str.replace(i, '');
+    }
+  });
+
+  return str ? false : true;
+
+};
+
+
+
+
+
+
+
+
+
+
+// console.log(check('((()))()', [['(', ')']]));
+// // const config = [['(', ')'], ['|', '|']];
