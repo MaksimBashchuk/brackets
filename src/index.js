e667ff51671a4cdeec79d72abeb69f3099a9f9cd
Maksim Bashchuk <MaksimBashchuk@gmail.com>
2020-10-07 13:02:55 +0300

    feat: add brackets solution

--- src/index.js
+++ src/index.js
@@ -3,24 +3,14 @@ function check(str, bracketsConfig) {
   let bracketsStr = [];
   bracketsConfig.forEach(i => bracketsStr.push(i.join('')));
 
-  bracketsStr.forEach(i => {
-    while (str.includes(i)) {
-      str = str.replace(i, '');
-    }
-  });
-
+  for (let i = 0; i < str.length * 5; i++) {
+    
+    bracketsStr.forEach(i => {
+      while (str.includes(i)) {
+        str = str.replace(i, '');
+      }
+    });
+    
+  }
   return str ? false : true;
-
 };
-
-
-
-
-
-
-
-
-
-
-// console.log(check('((()))()', [['(', ')']]));
-// // const config = [['(', ')'], ['|', '|']];
