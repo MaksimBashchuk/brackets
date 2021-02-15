ec5143919c3dfcbe70ea440d90cdc984c32b99c1
Maksim Bashchuk <MaksimBashchuk@gmail.com>
2021-02-15 10:54:29 +0300

    refactor: change cycle

--- src/index.js
+++ src/index.js
@@ -3,14 +3,17 @@ function check(str, bracketsConfig) {
   let bracketsStr = [];
   bracketsConfig.forEach(i => bracketsStr.push(i.join('')));
 
-  for (let i = 0; i < str.length * 5; i++) {
+  for (let i = 0; i < str.length; i++) {
     
     bracketsStr.forEach(i => {
       while (str.includes(i)) {
         str = str.replace(i, '');
       }
     });
-    
+
+    if (bracketsStr.some(elem => str.includes(elem))) {
+      i--;
+    }
   }
   return str ? false : true;
 };
